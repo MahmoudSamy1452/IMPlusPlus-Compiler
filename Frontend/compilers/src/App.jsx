e2f79075958b085e3c4fbac0adca2518a939e6ba
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-05-16 22:38:24 +0300

    Initial commit for sidebar

--- Frontend/compilers/src/App.jsx
+++ Frontend/compilers/src/App.jsx
@@ -29,30 +29,34 @@ function App() {
 
   return (
 
-    <div className='overflow-hidden h-screen w-full flex flex-col items-center text-left bg-[#171717]'>
-      <div className='w-full mt-[6px] flex items-center flex-row h-[65px] bg-[#252526]'>
-        <h1 className='ml-6 text-gray-300 text-[14px] tracking-wider font-light'>IM++</h1>
-        <svg className='w-4 h-[14px] text-gray-200' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-        </svg>
-        <h1 className='text-gray-300 text-[14px] tracking-wider font-light'>src</h1>
-        <svg className='w-4 h-[14px] text-gray-200' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-        </svg>
-        <h1 className='text-gray-300 text-[14px] tracking-wider font-light'>index.imx</h1>
-        <div onClick={handleCompile} className='flex ml-4 bg-[#171717] hover:bg-inherit pr-2 h-full flex-row cursor-pointer items-center'>
-          <PlayArrowOutlinedIcon className=' mt-[1px] text-gray-300' />
-          <h1 className='text-gray-300 text-[13px] ml-[1px] mt-[1px]'>Run</h1>
+    <div className='flex'>
+      {/* <div>
+        <div className='w-[250px] h-screen bg-[#1E1E1E]'/>
+      </div> */}
+      <div className='overflow-hidden h-screen w-full flex flex-col items-center text-left bg-[#171717]'>
+        <div className='w-full mt-[6px] flex items-center flex-row h-[65px] bg-[#252526]'>
+          <h1 className='ml-6 text-gray-300 text-[14px] tracking-wider font-light'>IM++</h1>
+          <svg className='w-4 h-[14px] text-gray-200' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+          </svg>
+          <h1 className='text-gray-300 text-[14px] tracking-wider font-light'>src</h1>
+          <svg className='w-4 h-[14px] text-gray-200' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+          </svg>
+          <h1 className='text-gray-300 text-[14px] tracking-wider font-light'>index.imx</h1>
+          <div onClick={handleCompile} className='flex ml-4 bg-[#171717] hover:bg-inherit pr-2 h-full flex-row cursor-pointer items-center'>
+            <PlayArrowOutlinedIcon className=' mt-[1px] text-gray-300' />
+            <h1 className='text-gray-300 text-[13px] ml-[1px] mt-[1px]'>Run</h1>
+          </div>
+        </div>
+        <Editor codemirrorRef={codemirrorRef[0]} />
+        <div className='flex flex-row border-t h-fit min-h-[335px] w-full'>
+          <Output title={"Quadruples"} codemirrorRef={codemirrorRef[1]} />
+          <Output title={"Errors"} codemirrorRef={codemirrorRef[2]} />
+          <Output title={"Symbol Table"} codemirrorRef={codemirrorRef[3]} />
         </div>
       </div>
-      <Editor codemirrorRef={codemirrorRef[0]} />
-      <div className='flex flex-row border-t h-fit min-h-[335px] w-full'>
-        <Output title={"Quadruples"} codemirrorRef={codemirrorRef[1]} />
-        <Output title={"Errors"} codemirrorRef={codemirrorRef[2]} />
-        <Output title={"Symbol Table"} codemirrorRef={codemirrorRef[3]} />
-      </div>
     </div>
-
   )
 }
 
